1b30879906d5ea904db5aa0104c962814d490af5
Martin Muñoz <martin.munoz.a@gmail.com>
2023-08-18 22:16:40 -0400

    web

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -23,9 +23,9 @@ const RESOURCES = {"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "c86296fd855cd118d89d8ae8a38926bb",
-"/": "c86296fd855cd118d89d8ae8a38926bb",
-"main.dart.js": "62dc1f4d5025319c1701485809410e21",
+"index.html": "8bb6febc6270f99dd6fe4734f3062508",
+"/": "8bb6febc6270f99dd6fe4734f3062508",
+"main.dart.js": "ba95910bac83639ad1d5c0930266c074",
 "manifest.json": "fe607d0ebfa1462561ad73bad8608a8b",
 "version.json": "f30e96578e1591deef4cbe4811c4dad8"};
 // The application shell files that are downloaded before a service worker can
